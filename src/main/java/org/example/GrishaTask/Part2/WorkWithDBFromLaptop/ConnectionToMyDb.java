8963b586b24260ce6df3a906dd3e37b8d4b57260
Temka_laptop <Thomasvill16@inbox.ru>
2024-01-29 10:20:25 +0400

    /

--- src/main/java/org/example/GrishaTask/Part2/WorkWithDBFromLaptop/ConnectionToMyDb.java
+++ src/main/java/org/example/GrishaTask/Part2/WorkWithDBFromLaptop/ConnectionToMyDb.java
@@ -38,4 +38,5 @@ public class ConnectionToMyDb {
         return null;
 
     }
+
 }
--- src/main/java/org/example/Jusan/TasksForStepik.java
+++ src/main/java/org/example/Jusan/TasksForStepik.java
@@ -23,6 +23,35 @@ public class TasksForStepik {
     }
 
 
+    public static void reverse(int[] array) {
+        int[] arr = new int[array.length];
+        int count = 0;
+        for (int i = array.length - 1; i >= 0; i--) {
+            arr[count] = array[i];
+            count++;
+        }
+        // Update the original array with the reversed values
+        for (int i = 0; i < array.length; i++) {
+            array[i] = arr[i];
+        }
+    }
+
+    public static int[] join(int[] array1, int[] array2) {
+
+        int[] arr = new int[array1.length+array2.length];
+
+        for(int i =0; i<array1.length; i++)
+        {
+            arr[i] = array1[i];
+        }
+        for(int i =0; i<array2.length; i++)
+        {
+            arr[i+array1.length] = array2[i];
+        }
+
+
+        return arr;
+    }
 
 
 
